5c66f66c04663a0e41b2420d3e01dbc2069530e3
Hüseyin Hanci <04170000331@ogrenci.ege.edu.tr>
2022-03-16 22:52:22 +0300

    16.03

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+
+            isimler.Add("Hüseyin");
+            
+
+        }
+    }
+}
